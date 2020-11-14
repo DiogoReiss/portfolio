25973f7d3d298d7c344fefc9f868b543addda21d
DiogoReiss <diogommreis1@gmail.com>
2020-11-14 03:43:08 +0000

    update: Trying to ajust the text in index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,8 +11,8 @@ const IndexPage: NextPage = () => {
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/> 
       </Head>
       <Nav />
-      <div className="absolute max-w-lg max-h-lg inset-y-0 self-center my-24 md:my-32 lg:my-32 xl:my-40 right-0 mr-32">
-        <h1 className="font-header h-2 md:text-2xl lg:text-4xl xl:text-6xl text-white-color font-black text-right tracking-tight">Hi, Dear. I'm a <strong className="text-pink-color">fullstack programmer</strong>. Come see my projects<strong className="text-pink-color"> :).</strong></h1>
+      <div className="absolute max-w-lg max-h-lg inset-y-0 self-center my-24 md:my-20 lg:my-24 xl:my-40 right-0 mr-32">
+        <h1 className="font-header h-2 md:text-2xl lg:text-5xl xl:text-6xl text-white-color font-black text-right tracking-tight">Hi, Dear. I'm a <strong className="text-pink-color">fullstack programmer</strong>. Come see my projects<strong className="text-pink-color"> :).</strong></h1>
       </div>
       <footer className="absolute inset-x-0 bottom-0 p-2 bg-transparent">
         <p className='font-sans text-pink-color font-500 text-center bg-transparent'>website made with, nextjs, tailwindcss and lots of love</p>
