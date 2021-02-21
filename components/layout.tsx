8b45c25a9df50a87cb8e9a53f8fe54ab6d89eb6d
DiogoReiss <diogommreis1@gmail.com>
2021-02-21 02:41:09 +0000

    feature: creating a layout and nav components

---
+++ components/layout.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head';
+import Nav from './nav';
+
+export default function Layout({ children, title, actualPage }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,800;1,800&display=swap"
+          rel="stylesheet"
+        />
+        <meta
+          name="description"
+          content="Welcome to my website, here I'll talk about me"
+        />
+        <meta name="author" content="Diogo Reis" />
+      </Head>
+      <body className="flex bg-gray-color w-full h-screen">
+        <div className="absolute h-screen w-3 bg-background-color" />
+        <Nav actualPage={`${actualPage}`} />
+        {children}
+        <footer className="absolute inset-x-0 bottom-0 p-2 bg-transparent">
+          <p className="font-sans text-pink-color font-500 text-center bg-transparent">
+            website made with, nextjs, tailwindcss and lots of love
+          </p>
+        </footer>
+      </body>
+    </>
+  );
+}
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,29 +1,38 @@
-import { NextPage } from 'next'
-import Link from 'next/link'
+import Link from 'next/link';
+import { useState } from 'react';
 
 const links = [
   { href: '/', label: 'HOME' },
   { href: '/about', label: 'ABOUT ME' },
   { href: '/repositories', label: 'GITHUB' },
-]
+];
 
-const Nav: NextPage = () => {
+export default function Nav({ actualPage }) {
   return (
-    <nav className="bg-transparent">
-      <div className="absolute h-full w-2 inset-0 bg-pink-color"></div>
-      <ul className="flex justify-end p-8 mx-24 bg-transparent">
-        <ul className="flex items-end justify-end space-x-4 bg-transparent">
+    <nav className="flex flex-row h-24 z-50 w-full absolute">
+      <ul
+        className={`flex self-center font-header m-auto md:ml-10 md:text-xl md:font-extrabold`}
+      >
+        <ul className="grid grid-cols-3 space-x-4 text-center">
           {links.map(({ href, label }) => (
-            <li key={`${href}${label}`} className="bg-transparent">
+            <li key={`${href}${label}`}>
               <Link href={href}>
-                <a className="no-underline nav-link hover:text-white-color active:text-color-white bg-transparent">{label}</a>
+                <a
+                  className={`no-underline ${
+                    actualPage == label
+                      ? 'text-pink-color'
+                      : 'text-background-color'
+                  } text-md font-bold hover:${
+                    actualPage == label ? 'underline' : 'text-pink-color'
+                  }`}
+                >
+                  {label}
+                </a>
               </Link>
             </li>
           ))}
         </ul>
       </ul>
     </nav>
-  )
+  );
 }
-
-export default Nav
