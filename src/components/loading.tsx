4d1f5682f2638272d317cf1a5f4dcabc72b36bc4
DiogoReiss <diogommreis1@gmail.com>
2021-03-17 22:52:30 +0000

    fixing

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -1,8 +1,12 @@
-import CircularProgress from '@material-ui/core/CircularProgress';
 import { connect } from 'react-redux';
 
 let Loading = ({ loading }) =>
-  loading ? <CircularProgress color="secondary" /> : null;
+  loading ? (
+    <img
+      className="animate-spin"
+      src="https://img.icons8.com/fluent-systems-regular/24/000000/loading-circle--v1.png"
+    />
+  ) : null;
 const mapStateToProps = (state) => ({ loading: state.loading });
 Loading = connect(mapStateToProps, null)(Loading);
 
