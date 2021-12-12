09f1e4eb4377620b952dfced10f2353ca4af0676
Diogo Reis <diogommreis1@gmail.com>
2021-12-12 23:05:08 +0000

    new: introducing Layout component on index page, modifying Navbar and global styles

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -3,21 +3,32 @@ import styles from "./Navbar.module.css";
 import Link from "next/link";
 
 const Navbar = () => {
-  const pageOptions: string[] = ["home", "projects", "blog", "curriculum"];
+  type pageOptions = Array<{
+    route: string;
+    name: string;
+  }>;
+  const pageOptions: pageOptions = [
+    { route: "/", name: "home" },
+    { route: "/projects", name: "projects" },
+    { route: "/blog", name: "blog" },
+    { route: "/curriculum", name: "curriculum" },
+  ];
 
   return (
     <div className={styles.container}>
-      <Image
-        src="/samurai.svg"
-        alt="page logo"
-        id="nav-logo"
-        width={70}
-        height={70}
-      />
+      <a href="/">
+        <Image
+          src="/samurai.svg"
+          alt="page logo"
+          id="nav-logo"
+          width={70}
+          height={70}
+        />
+      </a>
       <div className={styles.pageOptions}>
         {pageOptions.map((route, index) => (
-          <Link key={index} href={`#${route}`}>
-            {route}
+          <Link key={index} href={route.route}>
+            {route.name}
           </Link>
         ))}
       </div>
